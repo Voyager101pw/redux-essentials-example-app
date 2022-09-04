4626de9d3d8a840fa200ac271a38ca95e2036ffc
Voyager101pw <voyager101sirius@gmail.com>
2022-09-05 04:17:37 +0500

    Configuring store (rtk-query part-7)

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from '../features/posts/postsSlice.js';
 import usersReducer from '../features/users/usersSlice.js';
 import notificationsSlice from '../features/notifications/notificationsSlice.js';
+import { apiSlice } from '../features/api/apiSlice';
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
     notifications: notificationsSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(apiSlice.middleware)
+    // - это сделано, чтобы сохранить предыдущие redux-thunk (fetchPost fetchUsers fetchNotif...)
+    // из каждого среза (слайса) и в тоже время применить вновь созданный мидлвар из apiSlice
 })
