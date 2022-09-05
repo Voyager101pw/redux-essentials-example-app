b0d1bf460919dc3c8d61cbdd5b40252179707111
Voyager101pw <voyager101sirius@gmail.com>
2022-09-06 02:12:07 +0500

    Added mutation endpoint addNewPost (rtk-query part-7)

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,8 +5,8 @@ export const apiSlice = createApi({
   reducerPath: 'api', // store: state.api
 
   // Все наши запросы будут иметь URL-адреса, начинающиеся с «/fakeApi».
-  baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi'}), 
-  
+  baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+
   // «Конечные точки» представляют собой операции и запросы для этого сервера.
   // По умолчанию конечные точки запроса будут использовать GET HTTP-запрос
   // Для переопределения, вернуть объект, типа: {url: '/posts', method: 'POST', body: newPost} вместо самой строки URL
@@ -16,13 +16,27 @@ export const apiSlice = createApi({
     }),
     getPost: builder.query({
       query: (postId) => `/posts/${postId}`,
+    }),
+    addNewPost: builder.mutation({
+      query: (initialPost) => ({
+        url: '/posts',
+        method: 'POST',
+        body: initialPost // передаем полезную нагрузку (новый пост) в тело запроса
+      })
+      // Здесь наша quer yопция возвращает объект, содержащий {url, method, body}.
+      // Поскольку мы используем fetchBaseQuery для выполнения запросов, body поле 
+      // будет автоматически сериализовано для нас в формате JSON.
     })
   }),
 });
 
 // Экспортируем автоматически сгенерированный хук для конечной точки запроса `getPosts`
 // Ведь RTKQ-react автоматически генерирует хуки для каждой созданной нами конечной точки!
-export const { useGetPostsQuery, useGetPostQuery } = apiSlice;
+export const {
+  useGetPostsQuery,
+  useGetPostQuery,
+  useAddNewPostMutation // endpoint addNewPost: builder. > mutation <
+} = apiSlice;
 
 
 // Почему именно useGetPostsQuery ?
